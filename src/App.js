cf04a2c3458bea7e90532e3f2f4b7c9196fc54ab
pavan <pavankumarpatnala96@gmail.com>
2023-03-02 05:28:01 +0000

    pavan nxt

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,68 @@
+// Write your JS code here
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="nav-content">
+      <div className="nav-bar-mobile-logo-container">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+        />
+
+        <button type="button" className="nav-mobile-btn">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+            alt="nav logout"
+            className="nav-bar-img"
+          />
+        </button>
+      </div>
+
+      <div className="nav-content nav-bar-large-container">
+        <img
+          className="website-logo"
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+        />
+        <ul className="nav-menu">
+          <li className="nav-menu-item">Home</li>
+          <li className="nav-menu-item">Products</li>
+          <li className="nav-menu-item">Cart</li>
+        </ul>
+        <button type="button" className="logout-desktop-btn">
+          Logout
+        </button>
+      </div>
+    </div>
+    <div className="nav-menu-mobile">
+      <ul className="nav-menu-list-mobile">
+        <li className="nav-menu-item-mobile">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+            alt="nav home"
+            className="nav-bar-img"
+          />
+        </li>
+
+        <li className="nav-menu-item-mobile">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+            alt="nav products"
+            className="nav-bar-img"
+          />
+        </li>
+
+        <li className="nav-menu-item-mobile">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+            alt="nav cart"
+            className="nav-bar-img"
+          />
+        </li>
+      </ul>
+    </div>
+  </nav>
+)
+export default Header
